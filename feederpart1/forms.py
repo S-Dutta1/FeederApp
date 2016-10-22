e6d7c191d42153637bc629410d28832e43e434bd
SRIJIT DUTTA <srijit@cse.iitb.ac.in>
2016-10-22 19:59:27 -0400

    forms done

---
+++ feederpart1/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Post
+
+
+class ContactForm(forms.Form):
+    user_name = forms.CharField(,max_length=100,required=False)
+    user_password = forms.CharField(max_length=100,required=False)
+  
