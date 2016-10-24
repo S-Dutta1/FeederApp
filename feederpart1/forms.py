05be98ee9ab04e884ee198b73298354cad9d8dfa
Raunaque97 <rpatra1997@gmail.com>
2016-10-24 20:41:01 +0530

    changes in models

--- feederpart1/forms.py
+++ feederpart1/forms.py
@@ -4,4 +4,6 @@ from .models import Post
 
 class ContactForm(forms.Form):
     user_name = forms.CharField(,max_length=100,required=True)
-    user_password = forms.CharField(max_length=100,required=True)
+    # user_password = forms.CharField(max_length=100,required=True)
+    user_password = forms.CharField(widget = forms.PasswordInput())
+	
--- feederpart1/models.py
+++ feederpart1/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	username=models.CharField(max_length=50)
+	fullname=models.CharField(max_length=50)
+	instiname=models.CharField(max_length=50)
+	password=models.CharField(max_length=20)
+	email=models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.username
+	# class Meta:
+	# 	db_table=u'LoginData'
+	
+	# def __unicode__(self):
+	# 	return u"%s %s"%(self.username,self.password)
+	
